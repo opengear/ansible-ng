078828a35203f3980311abeecb3982f588289da8
Lachlan Archibald <lachlan.archibald@opengear.com>
2026-05-08 16:16:50 +1000

    test(om_facts): Add unit test test_om_facts[users]
    This patch adds the unit test for om_facts module which currently covers
    users. Each test is required to set its own fixtures for the facts that
    it expects to be able to return.

---
+++ opengear/om/tests/unit/modules/network/om/test_om_facts.py
@@ -0,0 +1,70 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible_collections.opengear.om.tests.unit.compat.mock import patch
+from ansible_collections.opengear.om.plugins.modules import om_facts
+from ansible_collections.opengear.om.tests.unit.modules.utils import set_module_args
+from .om_module import TestOmModule, load_fixture
+
+
+class TestOmFactsModule(TestOmModule):
+
+    module = om_facts
+
+    def setUp(self):
+        super(TestOmFactsModule, self).setUp()
+        self.maxDiff = None
+
+        self.mock_get_device_data = patch(
+            "ansible_collections.opengear.om.plugins.module_utils.network.om."
+            "facts.users.users.UsersFacts.get_device_data"
+        )
+        self.get_device_data = self.mock_get_device_data.start()
+
+        self.mock_connection = patch(
+            "ansible_collections.opengear.om.plugins.module_utils.network.om."
+            "config.base.Connection"
+        )
+        self.connection = self.mock_connection.start()
+
+    def tearDown(self):
+        super(TestOmFactsModule, self).tearDown()
+        self.mock_get_device_data.stop()
+        self.mock_connection.stop()
+
+    def load_fixtures(self, commands=None):
+        # Allow each test to configure its own fixtures
+        pass
+
+    def test_om_facts_gather_users(self):
+        self.get_device_data.side_effect = lambda *args, **kwargs: load_fixture(
+            "om_users_config.cfg"
+        )
+
+        set_module_args({
+            'gather_network_resources': ['users'],
+        })
+
+        result = self.execute_module(changed=False)
+
+        self.assertIn('ansible_facts', result)
+        self.assertIn('ansible_network_resources', result['ansible_facts'])
+        self.assertIn('users', result['ansible_facts']['ansible_network_resources'])
+
+        users = result['ansible_facts']['ansible_network_resources']['users']
+        self.assertEqual(len(users), 2)
+        self.assertEqual(users[0]['username'], 'user1')
+        self.assertEqual(users[1]['username'], 'user2')
+
+    def test_om_facts_gather_users_empty(self):
+        # Don't load any fixtures
+        set_module_args({
+            'gather_network_resources': ['users'],
+        })
+
+        result = self.execute_module(changed=False)
+
+        self.assertIn('ansible_facts', result)
+        users = result['ansible_facts']['ansible_network_resources'].get('users')
+        self.assertFalse(users)

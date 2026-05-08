6482765f486d8f1a852a7e28cb22c86d0c1f760b
Lachlan Archibald <lachlan.archibald@opengear.com>
2026-05-08 17:57:07 +1000

    test(om_facts): Require individual mocks for each module
    The om_facts unit tests now require individual mocks for each module to
    be configured, given that each test has different requirements for what
    data needs to be available.

--- opengear/om/tests/unit/modules/network/om/test_om_facts.py
+++ opengear/om/tests/unit/modules/network/om/test_om_facts.py
@@ -16,12 +16,6 @@ class TestOmFactsModule(TestOmModule):
         super(TestOmFactsModule, self).setUp()
         self.maxDiff = None
 
-        self.mock_get_device_data = patch(
-            "ansible_collections.opengear.om.plugins.module_utils.network.om."
-            "facts.users.users.UsersFacts.get_device_data"
-        )
-        self.get_device_data = self.mock_get_device_data.start()
-
         self.mock_connection = patch(
             "ansible_collections.opengear.om.plugins.module_utils.network.om."
             "config.base.Connection"
@@ -30,7 +24,6 @@ class TestOmFactsModule(TestOmModule):
 
     def tearDown(self):
         super(TestOmFactsModule, self).tearDown()
-        self.mock_get_device_data.stop()
         self.mock_connection.stop()
 
     def load_fixtures(self, commands=None):
@@ -38,15 +31,18 @@ class TestOmFactsModule(TestOmModule):
         pass
 
     def test_om_facts_gather_users(self):
-        self.get_device_data.side_effect = lambda *args, **kwargs: load_fixture(
+        mock = patch(
+            "ansible_collections.opengear.om.plugins.module_utils.network.om."
+            "facts.users.users.UsersFacts.get_device_data"
+        )
+        get_data = mock.start()
+        get_data.side_effect = lambda *args, **kwargs: load_fixture(
             "om_users_config.cfg"
         )
 
-        set_module_args({
-            'gather_network_resources': ['users'],
-        })
-
+        set_module_args({'gather_network_resources': ['users']})
         result = self.execute_module(changed=False)
+        mock.stop()
 
         self.assertIn('ansible_facts', result)
         self.assertIn('ansible_network_resources', result['ansible_facts'])
@@ -58,32 +54,34 @@ class TestOmFactsModule(TestOmModule):
         self.assertEqual(users[1]['username'], 'user2')
 
     def test_om_facts_gather_users_empty(self):
-        # Don't load any fixtures
-        set_module_args({
-            'gather_network_resources': ['users'],
-        })
+        mock = patch(
+            "ansible_collections.opengear.om.plugins.module_utils.network.om."
+            "facts.users.users.UsersFacts.get_device_data"
+        )
+        get_data = mock.start()
+        get_data.side_effect = lambda *args, **kwargs: []   # no data
 
+        set_module_args({'gather_network_resources': ['users']})
         result = self.execute_module(changed=False)
+        mock.stop()
 
         self.assertIn('ansible_facts', result)
         users = result['ansible_facts']['ansible_network_resources'].get('users')
         self.assertFalse(users)
 
     def test_om_facts_gather_groups(self):
-        self.get_device_data.side_effect = lambda *args, **kwargs: load_fixture(
-            "om_groups_config.cfg"
-        )
-
-        set_module_args({
-            'gather_network_resources': ['groups'],
-        })
-
-        self.mock_get_groups_data = patch(
+        mock = patch(
             "ansible_collections.opengear.om.plugins.module_utils.network.om."
             "facts.groups.groups.GroupsFacts.get_device_data"
         )
+        get_data = mock.start()
+        get_data.side_effect = lambda *args, **kwargs: load_fixture(
+            "om_groups_config.cfg"
+        )
 
+        set_module_args({'gather_network_resources': ['groups']})
         result = self.execute_module(changed=False)
+        mock.stop()
 
         self.assertIn('ansible_facts', result)
         self.assertIn('groups', result['ansible_facts']['ansible_network_resources'])
@@ -92,5 +90,3 @@ class TestOmFactsModule(TestOmModule):
         self.assertEqual(len(groups), 2)
         self.assertEqual(groups[0]['groupname'], 'admin')
         self.assertEqual(groups[1]['groupname'], 'netgrp')
-
-        self.mock_get_groups_data.stop()

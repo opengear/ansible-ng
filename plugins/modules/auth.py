dbcf34aa6fab53baa68b5497aeb43e2bef5d4877
Lachlan Archibald <lachlan.archibald@opengear.com>
2026-06-12 11:24:58 +1000

    fix: Replace deprecated warnings usage in exit_json
    Replace passing warnings directly to exit_json with module.warn(),
    which will become an error in ansible-core 2.23.

--- plugins/modules/auth.py
+++ plugins/modules/auth.py
@@ -190,6 +190,8 @@ def main():
                            supports_check_mode=True)
 
     result = Auth(module).execute_module()
+    for warning in result.pop('warnings', []):
+        module.warn(warning)
     module.exit_json(**result)
 
 
